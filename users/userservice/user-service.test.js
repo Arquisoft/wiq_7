22f808f2105a82e38164de9c45ea19d1da703bbb
angelalvaigle <angelalvaigle@gmail.com>
2024-11-27 18:21:13 +0100

    unit-test fix (copy utils in dockerfile)

--- users/userservice/user-service.test.js
+++ users/userservice/user-service.test.js
@@ -8,7 +8,7 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
   process.env.MONGODB_URI = mongoUri;
-  app = require('./user-service');
+  app = (await import('./user-service.js')).default; // Import app dynamically to ensure MONGODB_URI is set
 });
 
 afterAll(async () => {
