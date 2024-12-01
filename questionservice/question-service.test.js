a26ae5b05083907c164d91f8f5ccc27b3cdf7ce4
angelalvaigle <angelalvaigle@gmail.com>
2024-12-01 19:06:02 +0100

    stat- and question-services tests added

---
+++ questionservice/question-service.test.js
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongoServer;
+let app;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  process.env.MONGODB_URI = mongoUri;
+  app = (await import('./question-service.js')).default; // Import app dynamically to ensure MONGODB_URI is set
+});
+
+afterAll(async () => {
+  app.close();
+  await mongoServer.stop();
+});
+
+describe('Question Service', () => {
+  it('should add a new question on POST /addquestion', async () => {
+    const newQuestion = {
+      type: 'testType',
+      name: 'testName',
+      path: 'testPath',
+      right: 'testRight',
+      wrong1: 'testWrong1',
+      wrong2: 'testWrong2',
+      wrong3: 'testWrong3',
+    };
+
+    const response = await request(app).post('/addstst').send(newQuestion);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('userId', '507f1f77bcf86cd799439011');
+  });
+});
---
+++ statservice/stat-service.test.js
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongoServer;
+let app;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  process.env.MONGODB_URI = mongoUri;
+  app = (await import('./stat-service.js')).default; // Import app dynamically to ensure MONGODB_URI is set
+});
+
+afterAll(async () => {
+  app.close();
+  await mongoServer.stop();
+});
+
+describe('Stat Service', () => {
+  it('should add a new stat on POST /addstat', async () => {
+    const newStat = {
+      userId: '507f1f77bcf86cd799439011',
+      gameId: 'testGameId',
+      questionId: 'testQuestionId',
+      right: false,
+      time: 10,
+      points: 0,
+    };
+
+    const response = await request(app).post('/addstst').send(newStat);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('userId', '507f1f77bcf86cd799439011');
+  });
+});
