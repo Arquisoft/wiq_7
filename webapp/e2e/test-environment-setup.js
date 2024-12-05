08f8456379d5a2aedebd908c13cf8cef19eec1f8
angelalvaigle <angelalvaigle@gmail.com>
2024-12-05 01:56:52 +0100

    1st release e2e test fix

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -1,5 +1,4 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 let mongoserver;
 let userservice;
@@ -7,13 +6,15 @@ let authservice;
 let gatewayservice;
 
 async function startServer() {
-    console.log('Starting MongoDB memory server...');
-    mongoserver = await MongoMemoryServer.create();
-    const mongoUri = mongoserver.getUri();
-    process.env.MONGODB_URI = mongoUri;
-    userservice = await require("../../users/userservice/user-service");
-    authservice = await require("../../users/authservice/auth-service");
-    gatewayservice = await require("../../gatewayservice/gateway-service");
-  }
+  console.log('Starting MongoDB memory server...');
+  mongoserver = await MongoMemoryServer.create();
+  const mongoUri = mongoserver.getUri();
+  process.env.MONGODB_URI = mongoUri;
 
-  startServer();
+  userservice = (await import('../../users/userservice/user-service')).default;
+  authservice = (await import('../../users/authservice/auth-service')).default;
+  gatewayservice = (await import('../../gatewayservice/gateway-service'))
+    .default;
+}
+
+startServer();
