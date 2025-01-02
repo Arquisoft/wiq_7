3fe2a67a9a36dac83fd226b83a870be0108f7139
angelalvaigle <angelalvaigle@gmail.com>
2025-01-02 09:09:27 +0100

    ranking & stats page test

---
+++ webapp/src/tests/Ranking.test.js
@@ -0,0 +1,15 @@
+// Ranking.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Ranking from '../pages/Ranking';
+
+test('renders the Ranking page heading', () => {
+  // Renderiza el componente
+  render(<Ranking />);
+
+  // Busca el elemento con el texto "Ranking Page"
+  const heading = screen.getByText(/Ranking Page/i);
+
+  // Comprueba que está en el documento
+  expect(heading).toBeInTheDocument();
+});
---
+++ webapp/src/tests/Stats.test.js
@@ -0,0 +1,15 @@
+// Stats.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Stats from '../pages//Stats';
+
+test('renders the Stats page heading', () => {
+  // Renderiza el componente
+  render(<Stats />);
+
+  // Busca el elemento con el texto "Stats Page"
+  const heading = screen.getByText(/Stats Page/i);
+
+  // Comprueba que está en el documento
+  expect(heading).toBeInTheDocument();
+});
